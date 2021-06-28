75827e7d0268d11f24ece20130177595879ca27b
shvkmr536 <60297403+shvkmr536@users.noreply.github.com>
2021-06-28 22:39:31 +0530

    Create web_build_Pollscm.groovy

---
+++ jobdsl/web_build/web_build_Pollscm.groovy
@@ -0,0 +1,20 @@
+pipelineJob('web_build_1') {
+  definition {
+    cpsScm {
+        scm {
+          git {
+            remote {
+              url ('https://github.com/shvkmr536/JCASC-jenkinsfile.git')
+              credentials('github')
+            }
+          }
+        }
+        scriptPath("web_build/Jenkinsfile")
+    }
+  }
+  configure { project ->
+    project / 'properties' / 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' / 'triggers' / 'hudson.triggers.SCMTrigger' {
+       'spec'('* * * * *')
+    }
+  }
+}
